aa0105cc1362306e668df43ec567ef54ad669b41
zrj <18162307073@163.com>
2024-04-05 10:52:50 +0800

    add elitenemy X-move

--- src/edu/hitsz/application/Game.java
+++ src/edu/hitsz/application/Game.java
@@ -341,7 +341,7 @@ public class Game extends JPanel {
                     enemyAircrafts.add(new EliteEnemy(
                             (int) (Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())),
                             (int) (Math.random() * Main.WINDOW_HEIGHT * 0.05),
-                            0,
+                            5 * (r < 5?1:-1),
                             10,
                             60
                     ));
